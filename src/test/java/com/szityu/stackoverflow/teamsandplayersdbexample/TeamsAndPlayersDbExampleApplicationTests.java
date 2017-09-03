66bac85964d1879183d9c8425584e1bffe9ae5df
Fodor Szilard <szilard.l.fodor@gmail.com>
2017-09-03 08:35:09 +0200

    Initial project structure

---
+++ src/test/java/com/szityu/stackoverflow/teamsandplayersdbexample/TeamsAndPlayersDbExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szityu.stackoverflow.teamsandplayersdbexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamsAndPlayersDbExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
